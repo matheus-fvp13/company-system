6664b6d3c6c375635f8a060e4a5e74c92939a474
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-09-28 14:21:02 -0300

    add more actions in ClientController

--- src/main/java/edu/mfvp/company/controller/ClientController.java
+++ src/main/java/edu/mfvp/company/controller/ClientController.java
@@ -4,10 +4,9 @@ import edu.mfvp.company.dtos.ClientDtoRecord;
 import edu.mfvp.company.service.ClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
@@ -22,6 +21,27 @@ public class ClientController {
     @PostMapping
     public ResponseEntity<ClientDtoRecord> create(@RequestBody ClientDtoRecord clientDtoRecord) {
         return  new ResponseEntity<>(clientService.create(clientDtoRecord), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClientDtoRecord>> findAll() {
+        return ResponseEntity.ok().body(clientService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDtoRecord> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.findById(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ClientDtoRecord> update(@PathVariable Long id, @RequestBody ClientDtoRecord clientDtoRecord) {
+        return ResponseEntity.ok(clientService.update(id, clientDtoRecord));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        clientService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/edu/mfvp/company/service/impl/ClientServiceImp.java
+++ src/main/java/edu/mfvp/company/service/impl/ClientServiceImp.java
@@ -7,6 +7,8 @@ import edu.mfvp.company.service.ClientService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 public class ClientServiceImp implements ClientService {
@@ -25,21 +27,28 @@ public class ClientServiceImp implements ClientService {
 
     @Override
     public List<ClientDtoRecord> findAll() {
-        return null;
+        return clientRepository.findAll().stream().map(ClientDtoRecord::toClientDto).collect(Collectors.toList());
     }
 
     @Override
     public ClientDtoRecord findById(Long id) {
-        return null;
+        if(!clientRepository.existsById(id)) throw new NoSuchElementException("There is no a client with ID " + id);
+        Client client = clientRepository.findById(id).get();
+        return ClientDtoRecord.toClientDto(client);
     }
 
     @Override
     public ClientDtoRecord update(Long id, ClientDtoRecord clientDtoRecord) {
-        return null;
+        if(!clientRepository.existsById(id)) throw new NoSuchElementException("There is no a client with ID " + id);
+
+        Client client = clientDtoRecord.toClient();
+        client.setId(id);
+        return ClientDtoRecord.toClientDto(clientRepository.save(client));
     }
 
     @Override
     public void delete(Long id) {
-
+        if(!clientRepository.existsById(id)) throw new NoSuchElementException("There is no a client with ID " + id);
+        clientRepository.deleteById(id);
     }
 }
