0e451f4c7d69f29a7a386121cbffe143679a7626
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-09-28 08:49:15 -0300

    first commit

---
+++ src/test/java/edu/mfvp/company/CompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mfvp.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
